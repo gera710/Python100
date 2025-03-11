c3b9256669f77314efc860d9697850d9acfe451f
Oleksii Gerasymov <gera710@gmail.com>
2025-03-11 18:57:58 +0100

    Commmit with Day 1 and Day 2

---
+++ Day1/Day1.py
@@ -0,0 +1,38 @@
+print("Hello World!")
+print("Hello World!\nHey again!")
+# i = 3
+# i += 3
+# print(i)
+
+# print("Bye")
+#print("Hello " + input("What is your name? "))
+
+# print( len( input( "What is your name? " ) ) )
+
+# name = input( "What is your name? " )
+# print(name)
+
+# a = input("a: ")
+# b = input("b: ")
+
+# (a,b) = (b,a)
+# print("a: ", a)
+# print("b: ", b)
+
+print("Hello " + input("What is your name?") + "!")
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator")
+
+#2. Ask the user for the city that they grew up in.
+city_name = input("What's the name of the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet_name = input("What's your pet's name?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print("Your band name could be", city_name , pet_name)
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
---
+++ Tests/Func_Test.py
@@ -0,0 +1,6 @@
+def func(lst=[]):
+    lst += [0]
+    return lst
+print(func())
+print(func())
+print(func())
